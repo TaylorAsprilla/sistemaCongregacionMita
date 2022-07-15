925977b7919a39e5865c8969cb3dd54a4d80b034
Dav1497 <david.carrion@upr.com>
2022-07-14 23:23:28 -0400

    editar perfil cajitas bloqueadas, se ve la fecha, obrero y su pais en pdf, listado de obreros desde back

--- src/app/pages/informes/ver-informe/ver-informe.component.ts
+++ src/app/pages/informes/ver-informe/ver-informe.component.ts
@@ -1,12 +1,17 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { jsPDF } from 'jspdf';
+import { Subscription } from 'rxjs';
+import { PaisModel } from 'src/app/models/pais.model';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { PaisService } from 'src/app/services/pais/pais.service';
+import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 
 @Component({
   selector: 'app-ver-informe',
   templateUrl: './ver-informe.component.html',
   styleUrls: ['./ver-informe.component.css'],
 })
-export class VerInformeComponent implements OnInit {
+export class VerInformeComponent implements OnInit, OnDestroy {
   @ViewChild('content', { static: false }) el!: ElementRef;
 
   primerNombre: string;
@@ -14,7 +19,22 @@ export class VerInformeComponent implements OnInit {
   primerApellido: string;
   segundApellido: string;
   fechaActual: Date;
-  constructor() {}
+  trimestre: string;
+  public fechaSeleccionada = '';
+  public obreroSeleccionado = Object;
+  public paisSeleccionado: string;
+
+  items = [1, 2, 3];
+
+  public usuarios: UsuarioModel[] = [];
+  public usuarioSubscription: Subscription;
+
+  public paises: PaisModel[] = [];
+  public paisSubscription: Subscription;
+
+  public cargando: boolean = true;
+
+  constructor(private usuarioService: UsuarioService, private paisService: PaisService) {}
 
   ngOnInit(): void {
     this.primerNombre = sessionStorage.getItem('primerNombre');
@@ -22,28 +42,88 @@ export class VerInformeComponent implements OnInit {
     this.primerApellido = sessionStorage.getItem('primerApellido');
     this.segundApellido = sessionStorage.getItem('segundoApellido');
     this.fechaActual = new Date();
+    this.trimestre = '';
+    this.cargarUsuarios();
+    this.cargarPaises();
   }
 
-  makePDF(obrero: string, fecha: string) {
+  ngOnDestroy(): void {
+    this.usuarioSubscription?.unsubscribe();
+    this.paisSubscription?.unsubscribe();
+  }
+
+  cargarUsuarios() {
+    this.usuarioSubscription = this.usuarioService.listarTodosLosUsuarios().subscribe(({ totalUsuarios, usuarios }) => {
+      this.usuarios = usuarios;
+    });
+
+    // this.usuarioServices.listarUsuarios(this.paginaDesde).subscribe(({ totalUsuarios, usuarios }) => {
+    //   this.totalUsuarios = totalUsuarios;
+    //   this.usuarios = usuarios;
+    //   this.usuariosTemporales = usuarios;
+    //   this.cargando = false;
+    //   this.totalPaginas = Math.ceil(totalUsuarios / 5);
+    // });
+  }
+
+  cargarPaises() {
+    this.paisSubscription = this.paisService.listarPais().subscribe((pais) => {
+      this.paises = pais.filter((pais: PaisModel) => pais.estado === true);
+    });
+  }
+
+  seleccionarObrero(obreroSeleccionado) {
+    this.obreroSeleccionado = obreroSeleccionado;
+    let paisNum = obreroSeleccionado['pais_id'];
+    let paisObject = this.paises.filter((pais: PaisModel) => pais.id === paisNum)[0];
+    this.paisSeleccionado = paisObject['pais'];
+  }
+
+  seleccionarTrimestre() {
+    let fecha = this.fechaSeleccionada.slice(5, 7);
+    let mes = parseInt(fecha);
+
+    if (mes >= 1 && mes <= 3) {
+      this.trimestre = 'enero, febrero y marzo';
+    }
+    if (mes >= 4 && mes <= 6) {
+      this.trimestre = 'abril, mayo y junio';
+    }
+    if (mes >= 7 && mes <= 9) {
+      this.trimestre = 'julio, agosto y septiembre';
+    }
+    if (mes >= 10 && mes <= 12) {
+      this.trimestre = 'octubre, noviembre y diciembre';
+    }
+  }
+
+  makePDF() {
     let pdf = new jsPDF('p', 'pt', 'a4');
-    // pdf.setFont('helvetica', 'bold');
-    // pdf.text('Congregación Mita Inc.', 20, 50, null, 'center');
-    // Texto insertado a mano
-    // pdf.text(fecha, 25, 25);
-    // pdf.text(obrero, 25, 35);
-    // pdf.save('informe.pdf');
+    let pWidth = pdf.internal.pageSize.width; // 595.28 is the width of a4
+    let srcWidth = document.getElementById('content').scrollWidth;
+    let margin = 18; // narrow margin - 1.27 cm (36);
+    let scale = (pWidth - margin * 2) / srcWidth;
+    // pdf.setFont('Times');
+    //pdf.addFont('ArialMS', 'Arial', 'normal');
 
-    // Guardar elemento html
-    // pdf.canvas.height = 80 * 11;
-    // pdf.canvas.width = 80 * 8.5;
-    console.log(obrero);
-    pdf.html(this.el.nativeElement, {
-      callback: (pdf) => {
-        // texto insertado
-        pdf.text(fecha, 25, 25);
-        pdf.text(obrero, 25, 35);
+    // pdf.html(this.el.nativeElement, {
+    //   callback: (pdf) => {
+    //     // texto insertado
+    //     pdf.text(fechaSeleccionada, 25, 25);
+    //     pdf.text(obreroSeleccionado, 25, 35);
 
-        pdf.save('informe.pdf');
+    //     pdf.save('informe.pdf');
+    //   },
+    // });
+    pdf.html(document.getElementById('content'), {
+      x: margin,
+      y: margin,
+      html2canvas: {
+        // insert html2canvas options here, e.g.
+        scale: scale,
+      },
+      callback: function () {
+        window.open(pdf.output('bloburl'));
       },
     });
   }
--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -10,17 +10,39 @@ import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 })
 export class PerfilComponent implements OnInit {
   isEdit: boolean = false;
-
+  public perfilForm: FormGroup;
   public usuario: UsuarioModel;
 
-  constructor(private usuarioService: UsuarioService) {
+  constructor(private usuarioService: UsuarioService, private formBuilder: FormBuilder) {
     this.usuario = usuarioService.usuario;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.perfilForm = this.formBuilder.group({
+      primerNombre: [this.usuario.primerNombre, [Validators.required, Validators.minLength(3)]],
+      segundoNombre: [this.usuario.segundoNombre, [Validators.minLength(3)]],
+      primerApellido: [this.usuario.primerApellido, [Validators.required, Validators.minLength(3)]],
+      segundoApellido: [this.usuario.segundoApellido, [Validators.minLength(3)]],
+      numeroDocumento: [this.usuario.numeroDocumento, [Validators.required]],
+      nacionalidad: [this.usuario.nacionalidad, [Validators.required, Validators.minLength(3)]],
+      email: [this.usuario.email, [Validators.required, Validators.email]],
+      numeroCelular: [this.usuario.numeroCelular, [Validators.minLength(3)]],
+      fechaNacimiento: [this.usuario.fechaNacimiento, [Validators.required]],
+      login: [this.usuario.login, []],
+      password: [this.usuario.password, []],
+      foto: [this.usuario.fotoUrl, []],
+      genero_id: [this.usuario.genero_id, [Validators.required]],
+      tipoDocumento_id: [this.usuario.tipoDocumento_id, [Validators.required]],
+      pais_id: [this.usuario.pais_id, [Validators.required]],
+    });
+  }
 
   toggleEdit() {
     this.isEdit = !this.isEdit;
     console.log(this.isEdit);
+  }
+
+  submit() {
+    this.toggleEdit();
   }
 }
