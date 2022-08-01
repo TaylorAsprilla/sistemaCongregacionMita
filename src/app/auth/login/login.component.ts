26704bdfafa3b379910f5793b80cf5971edf47e7
Taylor Asprilla <taylor.asprilla@gmail.com>
2022-08-01 17:27:31 -0500

    Ajustes Header

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -3,6 +3,8 @@ import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { LoginForm } from 'src/app/interfaces/login-form.interface';
+import { UsuarioModel } from 'src/app/models/usuario.model';
+import { Rutas } from 'src/app/routes/menu-items';
 import { UsuarioService } from 'src/app/services/usuario/usuario.service';
 import Swal from 'sweetalert2';
 
@@ -35,21 +37,26 @@ export class LoginComponent implements OnInit {
 
   login() {
     this.usuarioService.login(this.loginForm.value).subscribe(
-      (loginUsuario: LoginForm) => {
+      (loginUsuario: any) => {
+        const usuario: UsuarioModel = loginUsuario.usuario;
+
         if (this.loginForm.get('remember').value) {
           localStorage.setItem('login', this.loginForm.get('login').value);
         } else {
           localStorage.removeItem('login');
         }
         Swal.fire({
-          icon: 'success',
-          title: 'Bienvenido ' + this.loginForm.get('login').value,
+          showClass: {
+            popup: 'animate__animated animate__fadeIn animate__slower',
+          },
+          position: 'bottom-end',
+          html: `Bienvenido ${usuario.primerNombre} ${usuario.segundoNombre} ${usuario.primerApellido}  `,
           showConfirmButton: false,
           timer: 1500,
         });
 
         // Navegar al Dashboard
-        this.router.navigateByUrl('/');
+        this.router.navigateByUrl(`${Rutas.SISTEMA}/${Rutas.INICIO}`);
       },
       (err) => {
         Swal.fire({ icon: 'error', html: 'Usuario y/o contraseña inválida' });
