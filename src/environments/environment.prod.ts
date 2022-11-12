dc4747fadddc14fb1207fbf00b3ed15fd6501a5f
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2022-11-11 20:30:18 -0500

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  base_url: 'https://kromatest.pw/api',
+  // base_url: 'https://kromatest.pw/api',
+  base_url: 'Sistemacmi-env.eba-vznvs8zx.us-east-2.elasticbeanstalk.com/api',
 };
