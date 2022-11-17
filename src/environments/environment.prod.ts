b4ff5d1f8ea564c0d5e9a52dc08c0b200b0074bd
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2022-11-16 19:06:42 -0500

    Ruta de la api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   // base_url: 'https://kromatest.pw/api',
-  base_url: 'http://sistemacmi-env.eba-vznvs8zx.us-east-2.elasticbeanstalk.com/api',
+  base_url: 'http://api.cmar.live/api',
 };
