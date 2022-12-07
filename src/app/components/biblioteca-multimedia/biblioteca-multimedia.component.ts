4db9a6ebf3dbc3e91e323bed1f34e7d6ed8d982a
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2022-12-07 16:01:56 -0500

    Corrección de eventos

--- src/app/components/biblioteca-multimedia/biblioteca-multimedia.component.ts
+++ src/app/components/biblioteca-multimedia/biblioteca-multimedia.component.ts
@@ -1,17 +1,42 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { LinkEventoModel, TIPOEVENTO } from 'src/app/core/models/link-evento.model';
+import {
+  AfterViewInit,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { LinkEventoModel, PLATAFORMA, TIPOEVENTO } from 'src/app/core/models/link-evento.model';
 
 @Component({
   selector: 'app-biblioteca-multimedia',
   templateUrl: './biblioteca-multimedia.component.html',
   styleUrls: ['./biblioteca-multimedia.component.scss'],
 })
-export class BibliotecaMultimediaComponent implements OnInit {
+export class BibliotecaMultimediaComponent implements OnInit, AfterViewInit, OnDestroy {
   @Input() serviciosYvigilias: LinkEventoModel[] = [];
   @Input() titulo: string = '';
+  @Input() tipoDeEvento: TIPOEVENTO;
+  // @Input() servicios: LinkEventoModel[] = [];
+  // @Input() vigilias: LinkEventoModel[] = [];
 
-  constructor() {}
+  @ViewChild('templateYouTubePlayer') templateYouTubePlayer: ElementRef<HTMLDivElement>;
+  videoWidth: number | undefined;
+  videoHeight: number | undefined;
+
+  linkEventos: LinkEventoModel[] = [];
+
+  get PLATAFORMA() {
+    return PLATAFORMA;
+  }
+
+  get TIPOEVENTO() {
+    return TIPOEVENTO;
+  }
+
+  constructor(private changeDetectorRef: ChangeDetectorRef) {}
 
   ngOnInit(): void {
     const tag = document.createElement('script');
@@ -19,4 +44,20 @@ export class BibliotecaMultimediaComponent implements OnInit {
     tag.src = 'https://www.youtube.com/iframe_api';
     document.body.appendChild(tag);
   }
+
+  ngOnDestroy(): void {
+    window.removeEventListener('resize', this.onResize);
+  }
+
+  ngAfterViewInit(): void {
+    this.onResize();
+    window.addEventListener('resize', this.onResize);
+  }
+
+  onResize = (): void => {
+    // Automatically expand the video to fit the page up to 1200px x 720px
+    this.videoWidth = Math.min(this.templateYouTubePlayer.nativeElement.clientWidth, 1000);
+    this.videoHeight = this.videoWidth * 0.6;
+    this.changeDetectorRef.detectChanges();
+  };
 }
