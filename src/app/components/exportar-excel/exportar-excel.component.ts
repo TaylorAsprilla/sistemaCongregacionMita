602a4fc1a420bc2186f784c5035a31dacb1d9b5b
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2024-11-21 16:25:06 -0500

    Actualizacion de librerias

--- src/app/components/exportar-excel/exportar-excel.component.ts
+++ src/app/components/exportar-excel/exportar-excel.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input, OnInit } from '@angular/core';
-import * as XLSX from 'xlsx';
+import { Workbook } from 'exceljs';
+import * as fs from 'file-saver';
 import { formatDate } from '@angular/common';
 import { UsuariosPorCongregacionInterface } from 'src/app/core/interfaces/usuario.interface';
 
 @Component({
-    selector: 'app-exportar-excel',
-    templateUrl: './exportar-excel.component.html',
-    styleUrls: ['./exportar-excel.component.scss'],
-    standalone: true,
+  selector: 'app-exportar-excel',
+  templateUrl: './exportar-excel.component.html',
+  styleUrls: ['./exportar-excel.component.scss'],
+  standalone: true,
 })
 export class ExportarExcelComponent implements OnInit {
   @Input() usuarios: UsuariosPorCongregacionInterface[] = [];
@@ -18,15 +19,26 @@ export class ExportarExcelComponent implements OnInit {
   ngOnInit(): void {}
 
   exportarExcel(): void {
-    const ws: XLSX.WorkSheet = XLSX.utils.json_to_sheet(this.usuarios);
-    const wb: XLSX.WorkBook = XLSX.utils.book_new();
-    XLSX.utils.book_append_sheet(wb, ws, this.nombreArchivo);
+    const workbook = new Workbook();
+    const worksheet = workbook.addWorksheet('Usuarios');
+
+    // Agregar encabezados
+    worksheet.columns = Object.keys(this.usuarios[0]).map((key) => ({ header: key, key }));
+
+    // Agregar filas
+    this.usuarios.forEach((usuario) => {
+      worksheet.addRow(usuario);
+    });
 
     const format = 'dd/MM/yyyy';
     const myDate = new Date();
     const locale = 'en-US';
     const formattedDate = formatDate(myDate, format, locale);
     const filename = this.nombreArchivo + '_' + formattedDate + '.xlsx';
-    XLSX.writeFile(wb, filename);
+
+    workbook.xlsx.writeBuffer().then((data) => {
+      const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      fs.saveAs(blob, filename);
+    });
   }
 }
