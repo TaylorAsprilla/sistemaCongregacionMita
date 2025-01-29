e9ebd99e5aff79a64b08b42a19b9d5efa8fcde4f
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2025-01-28 23:15:06 -0500

    Mejora en el Iframe de Youtube

--- src/app/components/servicios-en-vivo/servicios-en-vivo.component.ts
+++ src/app/components/servicios-en-vivo/servicios-en-vivo.component.ts
@@ -12,18 +12,12 @@ import {
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { LinkEventoModel, PLATAFORMA, TIPOEVENTO_ID } from 'src/app/core/models/link-evento.model';
 
-import { YouTubePlayer } from '@angular/youtube-player';
-import { YoutubePipe } from '../../pipes/youtube/youtube.pipe';
-
 @Component({
-    selector: 'app-servicios-en-vivo',
-    templateUrl: './servicios-en-vivo.component.html',
-    styleUrls: ['./servicios-en-vivo.component.scss'],
-    standalone: true,
-    imports: [
-    YouTubePlayer,
-    YoutubePipe
-],
+  selector: 'app-servicios-en-vivo',
+  templateUrl: './servicios-en-vivo.component.html',
+  styleUrls: ['./servicios-en-vivo.component.scss'],
+  standalone: true,
+  imports: [],
 })
 export class ServiciosEnVivoComponent implements OnInit, OnChanges, OnDestroy {
   @Input() servicios: LinkEventoModel = null;
@@ -54,8 +48,14 @@ export class ServiciosEnVivoComponent implements OnInit, OnChanges, OnDestroy {
       const videos = changes['servicios'].currentValue as LinkEventoModel;
       if (videos.plataforma === PLATAFORMA.VIMEO) {
         this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(videos.link);
+      } else if (videos.plataforma === PLATAFORMA.YOUTUBE) {
+        const videoId = this.extractYouTubeVideoId(videos.link);
+        console.log('videoId', videoId);
+        this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(
+          `https://www.youtube.com/embed/${videoId}?autoplay=1`
+        );
       } else {
-        this.videoUrl = videos.link;
+        this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(videos.link);
       }
       this.servicioPlataforma = videos.plataforma;
       this.titulo = videos.titulo;
@@ -69,6 +69,13 @@ export class ServiciosEnVivoComponent implements OnInit, OnChanges, OnDestroy {
   ngAfterViewInit(): void {
     this.onResize();
     window.addEventListener('resize', this.onResize);
+  }
+
+  extractYouTubeVideoId(url: string): string {
+    const regExp =
+      /(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/|youtube\.com\/live\/)([a-zA-Z0-9_-]{11})/;
+    const match = url.match(regExp);
+    return match && match[1].length === 11 ? match[1] : '';
   }
 
   onResize = (): void => {
