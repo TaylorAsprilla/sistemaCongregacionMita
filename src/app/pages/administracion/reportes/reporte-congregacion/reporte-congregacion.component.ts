29b28a9ee69efc218ca2997f92be72d97682ccdc
Taylor Asprilla <54963612+TaylorAsprilla@users.noreply.github.com>
2025-02-05 18:24:33 -0500

    Reportes de congregaciones

---
+++ src/app/pages/administracion/reportes/reporte-congregacion/reporte-congregacion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reporte-congregacion',
+  standalone: true,
+  imports: [],
+  templateUrl: './reporte-congregacion.component.html',
+  styleUrl: './reporte-congregacion.component.scss',
+})
+export default class ReporteCongregacionComponent {}
